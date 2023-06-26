952e85754b2097fa6aa69793ffc8e8d04a3dc340
pcui1 <cuip0001@e.ntu.edu.sg>
2023-06-26 15:17:08 +0800

    directory

--- test/split_rhsi_job.py
+++ test/split_rhsi_job.py
@@ -32,7 +32,7 @@ with open("job_id.txt", "r") as f:  # read n_jobs and job_id from file
 
 print("job_id = ", job_id)
 
-tm = create_TBModel_from_openmx39("data/rhsi.scfout")
+tm = create_TBModel_from_openmx39("../../../data/rhsi.scfout")
 mesh_size = [150, 150, 150]
 nks = np.prod(mesh_size)
 brillouin_zone_volume = abs(np.linalg.det(tm.rlat))

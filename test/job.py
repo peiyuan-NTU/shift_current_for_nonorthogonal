4cebb000b92481a99bd8292e4f34221d1ab5d23e
pcui1 <cuip0001@e.ntu.edu.sg>
2023-06-27 16:05:38 +0800

    update parallel

--- test/job.py
+++ test/job.py
@@ -1,17 +1,5 @@
 import os
 import sys
-import subprocess
-
-# 1. create job directories
-# 2. write job id and n_jobs to file
-# 3. write python code
-# 4. write slurm script
-# 5. submit job
-
-
-python_code = """
-import os
-import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
 from src.shift_conductivity import get_shift_cond_k
@@ -52,7 +40,6 @@ epsilon = np.sqrt(0.1)
 n_cores = int(os.environ["SLURM_CPUS_PER_TASK"])
 pool = mp.Pool(n_cores)
 for k in mesh_for_node:
-    print("apply k = ", k)
     pool.apply_async(get_shift_cond_k,
                      args=(tm, alpha, beta, gamma, omega_s, fermi, k, epsilon),
                      callback=collect_result)
@@ -62,43 +49,3 @@ pool.join()
 # write results to file
 with open(str(job_id) + "results.txt", "w") as f:
     f.write(str(results))
-"""
-
-slurm_script = """#!/bin/bash
-
-#SBATCH --job-name=rhsi_shift_cond
-#SBATCH --output=rhsi.out
-#SBATCH --error=rhsi.err
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=48
-#SBATCH --exclusive
-#SBATCH --time=24:00:00
-#SBATCH --mem=100G
-#SBATCH --partition=short
-
-source /home/${USER}/.bashrc
-source activate mpi
-python job.py
-"""
-
-n_jobs = 100
-
-prefix = os.path.join("rhsi")
-for i in range(n_jobs):
-    dir_name = os.path.join(prefix, "job_" + str(i))
-    if not os.path.exists(dir_name):
-        os.makedirs(dir_name)
-        with open(dir_name + "/job_id.txt", "w") as f:  # write job id and n_jobs to file
-            f.write(str(n_jobs) + "\n")
-            f.write(str(i) + "\n")
-        with open(dir_name + "/job.py", "w") as f:  # write python code
-            f.write(python_code)
-        with open(dir_name + "/job.slurm", "w") as f:  # write slurm script
-            f.write(slurm_script)
-        cmd_output = subprocess.run(["sbatch", "job.slurm"], capture_output=True, text=True, cwd=dir_name)
-        print(cmd_output.stdout)
-
-
-# read job id from file
-# with open("job_id.txt", "r") as f:
-#     job_id = int(f.read())
